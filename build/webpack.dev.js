b6603163e2810ecb344c65a05b7525c1b706094f
echoGoddess <1363794619@qq.com>
2022-11-18 15:38:20 +0800

    本地环境不单独分离css

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -33,15 +32,10 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       // 定义环境变量
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    }),
-    // 将css样式从bundle中分离出来，打包成一个单独的css
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
+    })
     // 根据内容而不是根据模块ID来判断是否需要更新模块
     // 在使用该插件热更新css文件时，打包出来的filename必须是[name].css,
     // 因为根据ID的话，文件名是动态的，找不到css文件了
-    new webpack.HotModuleReplacementPlugin()
+    // new webpack.HotModuleReplacementPlugin()
   ]
 });
