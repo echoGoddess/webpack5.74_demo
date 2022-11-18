99b5233c1a938aaee86d7ea1930a91ea17731a4c
echoGoddess <1363794619@qq.com>
2022-11-18 15:40:39 +0800

    导入样式文件可正确编译，style中样式被忽略

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>hello vue3</div>
+  <div class="content">hello vue3</div>
 </template>
 <script>
+// TODO:导入样式文件可正确编译，style中样式被忽略
+// import "./style.scss";
+
 export default {};
 </script>
-<style></style>
+<style scoped lang="scss">
+.content {
+  color: red;
+}
+</style>
